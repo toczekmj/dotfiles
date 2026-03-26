a5e79787f54abd4fdee1e99786e632d03996e6bc
Michał Toczek <toczekmj@gmail.com>
2026-03-26 12:30:59 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+echo "===> Updating system"
+sudo pacman -Syu --noconfirm
+
+echo "===> Installing packages"
+packages=(
+  git
+  niri
+  stow
+  fish
+  neovim
+  kitty
+  rofi
+  waybar
+)
+
+sudo pacman -S --needed --noconfirm "${packages[@]}"
+
+echo "===> Cloning dotfiles"
+if [ ! -d "$HOME/.dotfiles" ]; then
+  git clone git@gitlab.com:toczekmj/arch-dotfiles.git ~/.dotfiles
+fi
+
+cd ~/.dotfiles
+
+echo "===> Applying dotfiles with stow"
+for dir in */; do
+  stow "${dir%/}"
+done
+
+echo "===> Setting fish as default shell"
+chsh -s /usr/bin/fish
+
+echo "===> Installation complete! Please restart your terminal."
